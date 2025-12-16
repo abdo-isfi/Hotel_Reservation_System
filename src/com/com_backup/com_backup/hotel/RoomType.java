72c7f90d4065ea58eace9e37916a9096e888c5a2
abdo-isfi <abderrahmaneisfi12@gmail.com>
2025-12-16 23:19:20 +0100

    Remove nested com_backup folder created during submodule conversion

--- src/com/com_backup/com_backup/hotel/RoomType.java
+++
@@ -1,7 +0,0 @@
-package com.hotel;
-
-public enum RoomType {
-    STANDARD_SUITE,
-    JUNIOR_SUITE,
-    MASTER_SUITE
-}
--- src/com/com_backup/hotel/HotelException.java
+++
@@ -1,7 +0,0 @@
-package com.hotel;
-
-public class HotelException extends RuntimeException {
-    public HotelException(String message) {
-        super(message);
-    }
-}
